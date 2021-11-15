b021920e16db2a104cc9c2e1a3c03c9095c8ba56
astrasb <10248744+astrasb@users.noreply.github.com>
2021-11-15 09:30:19 -0800

    Cleaning install packages

--- installpackages.R
+++ installpackages.R
@@ -16,7 +16,5 @@ remove.packages("patchwork", lib="~/R/win-library/4.1")
 devtools::install_github("thomasp85/patchwork")
 
 BiocManager::install(c("limma","edgeR","clusterProfiler"))
-BiocManager::install("clusterProfiler")
-
 
 pacman::p_load(shiny,shinyjs,shinyWidgets,htmltools,shinythemes,DT,Cairo,tidyverse,ggforce,gt,plotly,magrittr,ggthemes,gplots,svglite,Cairo,heatmaply,RColorBrewer,rcartocolor,openxlsx,egg,dendextend,vctrs,markdown)
